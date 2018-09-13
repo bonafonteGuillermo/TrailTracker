6f904524bdd43d87c5e36d92488203b252277323
Guillermo Bonafonte <guillermo.bonafontecriado@cognizant.com>
2018-09-13 15:39:53 +0200

    Added Camera Update Factory and polyline

--- TrailTrackerApp/app/src/main/java/app/demo/example/com/trailtracker/map/MapView.kt
+++ TrailTrackerApp/app/src/main/java/app/demo/example/com/trailtracker/map/MapView.kt
@@ -9,9 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import app.demo.example.com.trailtracker.R
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.*
+import com.google.android.gms.maps.model.RoundCap
+
 
 /**
  *
@@ -21,12 +25,13 @@ import com.google.android.gms.maps.model.PolylineOptions
  */
 class MapView(context: AppCompatActivity) : IMapView, OnMapReadyCallback {
 
+    private val widthPx = 40
+
     override var context: Context = context
     override var presenter: IMapPresenter? = null
     override fun constructView(): View = view
 
     private val mapView by lazy { view.findViewById<com.google.android.gms.maps.MapView>(R.id.map) }
-    private var map: GoogleMap? = null
     var view: View
 
     init {
@@ -44,13 +49,10 @@ class MapView(context: AppCompatActivity) : IMapView, OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap?) {
         googleMap ?: return
 
+        val routeLatLongArray = presenter?.getRouteLatLongArray()
 
-        with(googleMap) {
-            addPolyline(PolylineOptions().apply {
-                addAll(presenter?.getRouteLatLongArray())
-                color(Color.BLUE)
-            })
-        }
+        drawPolyLine(routeLatLongArray, googleMap)
+        centerMapCameraInRouteLocation(routeLatLongArray, googleMap)
     }
 
     override fun onStart() = mapView.onStart()
@@ -66,4 +68,37 @@ class MapView(context: AppCompatActivity) : IMapView, OnMapReadyCallback {
     override fun onSaveInstanceState(instanceState: Bundle?) = mapView.onSaveInstanceState(instanceState)
 
     override fun onLowMemory() = mapView.onLowMemory()
+
+    private fun centerMapCameraInRouteLocation(routeLatLongArray: ArrayList<LatLng>?, googleMap: GoogleMap) {
+        val builder = LatLngBounds.Builder()
+        for (latLong in routeLatLongArray!!.iterator()) {
+            builder.include(LatLng(latLong.latitude, latLong.longitude))
+        }
+        val bounds: LatLngBounds = builder.build()
+        val width = context.resources.displayMetrics.widthPixels
+        val height = context.resources.displayMetrics.heightPixels
+        val padding = (width * 0.12).toInt() // offset from edges of the map 12% of screen
+        val cu: CameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding)
+        val cuZoom: CameraUpdate = CameraUpdateFactory.zoomTo(15f)
+
+        try {
+            googleMap.moveCamera(cu)
+            googleMap.animateCamera(cu)
+            googleMap.moveCamera(cuZoom)
+        } catch (ex: Exception) {
+        }
+    }
+
+    private fun drawPolyLine(routeLatLongArray: ArrayList<LatLng>?, googleMap: GoogleMap) {
+        var polyLineOptions: PolylineOptions = PolylineOptions().apply {
+            addAll(routeLatLongArray)
+            color(Color.BLUE)
+            width(widthPx.toFloat())
+        }
+
+        var line = googleMap.addPolyline(polyLineOptions)
+        line.startCap = RoundCap()
+        line.endCap = RoundCap()
+        line.jointType = JointType.ROUND
+    }
 }
